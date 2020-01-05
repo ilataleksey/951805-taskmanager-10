fe4ee55e1f4e62589c38adc84cd62e1f2f217a70
ilataleksey <ilataleksey@gmail.com>
2020-01-05 20:50:08 +0300

    Обновляет логику генерации разметки с учетом классов, добавляет функцию переключения карточки на режим редактирования и обратно

--- src/main.js
+++ src/main.js
@@ -1,48 +1,63 @@
-import {createMenuTemplate} from './components/menu.js';
-import {createFilterTemplate} from './components/filter.js';
-import {createBoardTemplate} from './components/board.js';
-import {createTaskTemplate} from './components/task.js';
-import {createTaskEditTemplate} from './components/task-edit.js';
-import {createLoadMoreButtonTemplate} from './components/load-more-button.js';
+import MenuComponent from './components/menu.js';
+import FilterComponent from './components/filter.js';
+import BoardComponent from './components/board.js';
+import TaskComponent from './components/task.js';
+import TaskEditComponent from './components/task-edit.js';
+import LoadMoreButtonComponent from './components/load-more-button.js';
 import {generateTasks} from './mock/task.js';
 import {generateFilters} from './mock/filter.js';
+import {render, RenderPosition} from './utils.js';
 
 const TASK_COUNT = 22;
 const SHOWING_TASK_COUNT_ON_START = 8;
 const SHOWING_TASK_COUNT_BY_BUTTON = 8;
 
-const render = (container, template, place = `beforeend`) => {
-  container.insertAdjacentHTML(place, template);
+const renderTask = (task) => {
+  const taskEditComponent = new TaskEditComponent(task);
+  const taskComponent = new TaskComponent(task);
+
+  const editButton = taskComponent.getElement().querySelector(`.card__btn--edit`);
+  editButton.addEventListener(`click`, () => {
+    taskListElement.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
+  });
+
+  const editForm = taskEditComponent.getElement().querySelector(`form`);
+  editForm.addEventListener(`submit`, () => {
+    taskListElement.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
+  });
+
+  render(taskListElement, taskComponent.getElement(), RenderPosition.BEFOREEND);
 };
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-render(siteHeaderElement, createMenuTemplate());
+render(siteHeaderElement, new MenuComponent().getElement(), RenderPosition.BEFOREEND);
 
 const filters = generateFilters();
-render(siteMainElement, createFilterTemplate(filters));
-render(siteMainElement, createBoardTemplate());
+render(siteMainElement, new FilterComponent(filters).getElement(), RenderPosition.BEFOREEND);
 
-const taskListElement = siteMainElement.querySelector(`.board__tasks`);
+const boardComponent = new BoardComponent();
+render(siteMainElement, boardComponent.getElement(), RenderPosition.BEFOREEND);
+
+const taskListElement = boardComponent.getElement().querySelector(`.board__tasks`);
 const tasks = generateTasks(TASK_COUNT);
 
-render(taskListElement, createTaskEditTemplate(tasks[0]), `beforeend`);
 let showingTaskCount = SHOWING_TASK_COUNT_ON_START;
-tasks.slice(1, showingTaskCount).forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
+tasks.slice(0, showingTaskCount).forEach((task) => renderTask(task));
 
-const boardElement = siteMainElement.querySelector(`.board`);
-render(boardElement, createLoadMoreButtonTemplate());
+const loadMoreButtonComponent = new LoadMoreButtonComponent();
+render(boardComponent.getElement(), loadMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
 
-const loadMoreButton = boardElement.querySelector(`.load-more`);
-loadMoreButton.addEventListener(`click`, () => {
+loadMoreButtonComponent.getElement().addEventListener(`click`, () => {
   const prevTaskCount = showingTaskCount;
   showingTaskCount = showingTaskCount + SHOWING_TASK_COUNT_BY_BUTTON;
 
   tasks.slice(prevTaskCount, showingTaskCount)
-    .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
+    .forEach((task) => renderTask(task));
 
   if (showingTaskCount >= tasks.length) {
-    loadMoreButton.remove();
+    loadMoreButtonComponent.getElement().remove();
+    loadMoreButtonComponent.removeElement();
   }
 });
