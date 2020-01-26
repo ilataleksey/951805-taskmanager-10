c911a81824cdb8f95c7ba57a5e375c8bdf08b7c6
ilataleksey <ilataleksey@gmail.com>
2020-01-26 13:38:33 +0300

    Добавляет в модель таксков метод обновления таска

--- src/mock/task.js
+++ src/mock/task.js
@@ -60,6 +60,7 @@ const generateTask = () => {
   const dueDate = Math.random() > 0.5 ? null : getRandomDate();
 
   return {
+    id: String(new Date() + Math.random()),
     description: getRandomArrayItem(DESCRIPTION_ITEMS),
     dueDate,
     repeatingDays: dueDate ? DefaultRepeatingDays : generateRepeatingDays(),
