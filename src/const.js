c7d0113b262fe04b0abab2f9ca0582cb125fafd8
ilataleksey <ilataleksey@gmail.com>
2020-01-26 15:42:57 +0300

    Создает список фильтров, вспомогательные функции для фильтрации и основную функцию получения фильтрованного списка

--- src/const.js
+++ src/const.js
@@ -30,3 +30,13 @@ export const MONTH_NAMES = [
   `November`,
   `December`,
 ];
+
+export const FilterType = {
+  ALL: `all`,
+  ARCHIVE: `archive`,
+  FAVORITES: `favorites`,
+  OVERDUE: `overdue`,
+  REPEATING: `repeating`,
+  TAGS: `tags`,
+  TODAY: `today`,
+};
