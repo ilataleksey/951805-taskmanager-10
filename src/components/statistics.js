c74e7bd2ea60105775851a04b3a8bcf12a611538
ilataleksey <ilataleksey@gmail.com>
2020-01-26 21:34:37 +0300

    Превращает список задач в массив неповторяющихся цветов

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -61,7 +61,9 @@ const calculateBetweenDates = (from, to) => {
 };
 
 const renderColorsChart = (colorsCtx, tasks) => {
-
+  const colors = tasks
+    .map((task) => task.color)
+    .filter(getUniqItems);
 };
 
 const createStatisticsTemplate = ({tasks, dateFrom, dateTo}) => {
